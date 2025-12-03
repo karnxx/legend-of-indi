405534ea6772e990233743b930a95cc3adc48af4
Fastestkyo <fastestkyo@gmail.com>
2025-12-03 18:20:59 +0530

    Asd

--- data/element/ElementData.gd
+++ data/element/ElementData.gd
@@ -1,13 +1,18 @@
 extends Resource
 class_name ElementData
 
+# Define your elements as an enum
+enum ElementType { NORMAL, FIRE, WATER, GRASS }
+
+@export var element_type: ElementType  # unique type for this element
 @export var display_name: String
 @export var color: Color
-@export var picture:ImageTexture
+@export var picture: ImageTexture
 
-@export var strong_against: Array[ElementData]
-@export var weak_against: Array[ElementData]
-@export var resists: Array[ElementData]
-@export var immune_to: Array[ElementData]
+# Use enums for relationships instead of cross-referencing ElementData resources
+@export var strong_against: Array[ElementType] = []
+@export var weak_against: Array[ElementType] = []
+@export var resists: Array[ElementType] = []
+@export var immune_to: Array[ElementType] = []
 
-@export var tags: Array[String]
+@export var tags: Array[String] = []
